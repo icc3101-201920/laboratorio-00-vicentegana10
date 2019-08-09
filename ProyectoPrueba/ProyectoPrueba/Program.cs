98685c44a26fd097921a777ec4fe02dc2c2c3405
vicentegana10 <vgana@miuandes.cl>
2019-08-09 08:59:58 -0400

    first commit

---
+++ ProyectoPrueba/ProyectoPrueba/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a22a");
+            Console.ReadKey();
+        }
+    }
+}
